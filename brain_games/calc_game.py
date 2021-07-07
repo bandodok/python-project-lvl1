89ca6b2a7969180d236bbab8a2036d6ddce0b350
Dmitrij Svetov <bandodok@gmail.com>
2021-07-07 21:09:05 +0300

    calc-game linter error correction

--- brain_games/calc_game.py
+++ brain_games/calc_game.py
@@ -6,18 +6,18 @@ from brain_games.main_game import game
 def brain_calc():
     task = 'What is the result of the expression?'
     count = 0
-    questions = []
-    answers = []
+    que = []
+    ans = []
     while count < 3:
-        first_num = randint(0, 100)
-        second_num = randint(0, 100)
+        num1 = randint(0, 100)
+        num2 = randint(0, 100)
         sign = choice(('+', '-', '*'))
-        questions.append('{arg1} {arg2} {arg3}'.format(arg1=first_num, arg2=sign, arg3=second_num))
+        que.append('{ar1} {ar2} {ar3}'.format(ar1=num1, ar2=sign, ar3=num2))
         count += 1
         if sign == '+':
-            answers.append(str(first_num + second_num))
+            ans.append(str(num1 + num2))
         elif sign == '-':
-            answers.append(str(first_num - second_num))
+            ans.append(str(num1 - num2))
         elif sign == '*':
-            answers.append(str(first_num * second_num))
-    game(task, questions, answers)
+            ans.append(str(num1 * num2))
+    game(task, que, ans)
