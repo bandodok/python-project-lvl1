c3e595fdc221a4cd3cb24de33147df94f23f9be3
Dmitrij Svetov <bandodok@gmail.com>
2021-07-08 18:40:33 +0300

    added progression game

---
+++ brain_games/progression_game.py
@@ -0,0 +1,24 @@
+from random import randint
+
+from brain_games.main_game import game
+
+
+def brain_progression():
+    task = 'What number is missing in the progression?'
+    exp_ans = True
+    count = 0
+    que = []
+    ans = []
+    min_len = 5
+    max_len = 15
+    while count < 3:
+        start = randint(0, 100)
+        step = randint(1, 5)
+        end = randint(min_len, max_len) * step + start
+        progression = list(range(start, end, step))
+        index = randint(0, len(progression) - 1)
+        ans.append(str(progression[index]))
+        progression[index] = '..'
+        que.append(str(progression))
+        count += 1
+    game(task, que, ans, exp_ans)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,9 @@
+from brain_games.progression_game import brain_progression
+
+
+def main():
+    brain_progression()
+
+
+if __name__ == '__main__':
+    main()
