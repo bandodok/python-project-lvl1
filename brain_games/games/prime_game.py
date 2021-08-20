dcc99e3c134b5bb59af60bd76765d4251861c339
Dmitrij Svetov <bandodok@gmail.com>
2021-08-20 19:20:24 +0300

    update brain_prime

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -6,23 +6,12 @@ from brain_games.main_game import game
 def brain_prime():
     task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     exp_ans = True
-    count = 0
-    que = []
     ans = []
-    while count < 3:
-        count += 1
-        num = randint(2, 100)
-        que.append(num)
-        if num == 2:
-            ans.append('yes')
-            continue
-        elif num % 2 == 0:
+    que = [randint(2, 100) for _ in range(3)]
+    for num in que:
+        if num % 2 == 0 and num != 2 or num % 3 == 0:
             ans.append('no')
             continue
-        for index in range(3, num):
-            if num % index == 0:
-                ans.append('no')
-                break
-            ans.append('yes')
-            break
+        ans.append('yes')
+        continue
     game(task, que, ans, exp_ans)
