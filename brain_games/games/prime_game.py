6423ef7a915aa8958b377b2efdab9af8b2e99aae
Dmitrij Svetov <bandodok@gmail.com>
2021-08-20 20:19:44 +0300

    update brain_prime

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -9,9 +9,21 @@ def brain_prime():
     ans = []
     que = [randint(2, 100) for _ in range(3)]
     for num in que:
-        if num % 2 == 0 and num != 2 or num % 3 == 0:
-            ans.append('no')
+        if is_prime(num):
+            ans.append('yes')
             continue
-        ans.append('yes')
-        continue
+        ans.append('no')
     game(task, que, ans, exp_ans)
+
+
+def is_prime(num):
+    if num == 2:
+        return True
+    elif num % 2 == 0:
+        return False
+    for index in range(3, num):
+        if num % index == 0:
+            return False
+    return True
+
+brain_prime()
